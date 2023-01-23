be54ef6298070ae0a587a8f079d35ea642c461b8
a potato <jt2265658@gmail.com>
2023-01-22 17:33:43 -0700

    added lisa and fixed tab changing

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,7 @@ fetch(`https://cuddly-southern-bell.glitch.me/api/get12-1-1`)
                 let newCell = newRow.insertCell(-1);
 
                 if (j === 0) {
-                    newCell.innerHTML = `${i+1}`
+                    newCell.innerHTML = `${i + 1}`
                 }
                 else if (j === 1) {
                     newCell.innerHTML = `${InitialData[i].time}s`
@@ -39,12 +39,12 @@ fetch(`https://cuddly-southern-bell.glitch.me/api/get12-1-1`)
                 else if (j === 4) {
                     let teams = document.getElementsByTagName('td');
                     for (let k = 0; k < InitialData[i].team.length; k++) {
-                        
+
                         let icons = document.createElement('img');
                         icons.src = `assets/img/character-icon2/${InitialData[i].team[k]}.png`;
                         icons.width = '50';
                         icons.height = '50';
-                        teams[teams.length-1].append(icons);
+                        teams[teams.length - 1].append(icons);
                     }
 
                 }
@@ -63,50 +63,61 @@ function changeChamber(event) {
 
     //Create new table
     fetch(`https://cuddly-southern-bell.glitch.me/api/get${currentChamber.id}`)
-    .then(res => {
-        if (res.ok) {
-            console.log('success')
-        } else {
-            console.log('not successful')
-        }
-        return res.json()
-    })
-    .then(InitialData => {
-        console.log(InitialData)
-
-        // Wipe Table
-
-        let new_tbody = document.createElement('tbody');
-        
-        let tableRef = document.getElementsByTagName('tbody')[0];
-        
-
-        for (let i = 0; i < InitialData.length; i++) {
-            let newRow = new_tbody.insertRow();
-            for (let j = 0; j < 5; j++) {
-                let newCell = newRow.insertCell(-1);
-
-                if (j === 0) {
-                    newCell.innerHTML = `${i+1}`
-                }
-                else if (j === 1) {
-                    newCell.innerHTML = `${InitialData[i].time}`
-                }
-                else if (j === 2) {
-                    newCell.innerHTML = `${InitialData[i].name}`
-                }
-                else if (j === 3) {
-                    newCell.innerHTML = `${InitialData[i].link}`
-                }
-
+        .then(res => {
+            if (res.ok) {
+                console.log('success')
+            } else {
+                console.log('not successful')
             }
-        }
-        tableRef.parentElement.replaceChild(new_tbody, tableRef);
-        
-    })
-    .catch((error) => {
-        console.log(error);
-    })
+            return res.json()
+        })
+        .then(InitialData => {
+            console.log(InitialData)
+
+            // Wipe Table
+
+            let new_tbody = document.createElement('tbody');
+
+            let tableRef = document.getElementsByTagName('tbody')[0];
+
+            for (let i = 0; i < InitialData.length; i++) {
+                let newRow = tableRef.insertRow();
+                for (let j = 0; j < 5; j++) {
+                    let newCell = newRow.insertCell(-1);
+
+                    if (j === 0) {
+                        newCell.innerHTML = `${i + 1}`
+                    }
+                    else if (j === 1) {
+                        newCell.innerHTML = `${InitialData[i].time}s`
+                    }
+                    else if (j === 2) {
+                        newCell.innerHTML = `${InitialData[i].name}`
+                    }
+                    else if (j === 3) {
+                        newCell.innerHTML = `${InitialData[i].link}`
+                    }
+                    else if (j === 4) {
+                        let teams = document.getElementsByTagName('td');
+                        for (let k = 0; k < InitialData[i].team.length; k++) {
+
+                            let icons = document.createElement('img');
+                            icons.src = `assets/img/character-icon2/${InitialData[i].team[k]}.png`;
+                            icons.width = '50';
+                            icons.height = '50';
+                            teams[teams.length - 1].append(icons);
+                        }
+
+                    }
+
+                }
+            }
+            tableRef.parentElement.replaceChild(new_tbody, tableRef);
+
+        })
+        .catch((error) => {
+            console.log(error);
+        })
 }
 
 
