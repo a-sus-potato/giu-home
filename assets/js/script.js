0db37eeab0418d90e7b28be37b27acb808b815e3
a potato <jt2265658@gmail.com>
2023-01-24 19:29:14 -0700

    centered text, fixed link box

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,7 @@ fetch(`https://cuddly-southern-bell.glitch.me/api/get12-1-1`)
                 let newCell = newRow.insertCell(-1);
 
                 if (j === 0) {
-                    newCell.innerHTML = `${i+1}`
+                    newCell.innerHTML = `${i + 1}`
                 }
                 else if (j === 1) {
                     // let time = document.getElementsByTagName('td')[1];
@@ -39,17 +39,14 @@ fetch(`https://cuddly-southern-bell.glitch.me/api/get12-1-1`)
                 }
                 else if (j === 3) {
                     newCell.innerHTML = `
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-link-45deg" viewBox="0 0 16 16">
-                        <a xlink:href=${InitialData[i].link} target="_blank">
-                            <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z"/>
-                            <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z"/>
-                        </a>
-                    </svg>`
+                    <a href=${InitialData[i].link} target="_blank">
+                        <i class="bi bi-link-45deg"></i>
+                    </a>`
                 }
                 else if (j === 4) {
 
                     for (let k = 0; k < InitialData[i].team.length; k++) {
-                        
+
                         let icons = document.createElement('img');
                         icons.src = `assets/img/character-icon2/${InitialData[i].team[k]}.png`;
                         icons.width = '64';
@@ -73,67 +70,64 @@ function changeChamber(event) {
 
     //Create new table
     fetch(`https://cuddly-southern-bell.glitch.me/api/get${currentChamber.id}`)
-    .then(res => {
-        if (res.ok) {
-            //console.log('success')
-        } else {
-            console.log('not successful')
-        }
-        return res.json()
-    })
-    .then(InitialData => {
-        //console.log(InitialData)
+        .then(res => {
+            if (res.ok) {
+                //console.log('success')
+            } else {
+                console.log('not successful')
+            }
+            return res.json()
+        })
+        .then(InitialData => {
+            //console.log(InitialData)
 
-        // Wipe Table
+            // Wipe Table
 
-        let new_tbody = document.createElement('tbody');
-        
-        let tableRef = document.getElementsByTagName('tbody')[0];
-        
+            let new_tbody = document.createElement('tbody');
 
-        for (let i = 0; i < InitialData.length; i++) {
-            let newRow = new_tbody.insertRow();
-            for (let j = 0; j < 5; j++) {
-                let newCell = newRow.insertCell(-1);
+            let tableRef = document.getElementsByTagName('tbody')[0];
 
-                if (j === 0) {
-                    newCell.innerHTML = `${i+1}`
-                }
-                else if (j === 1) {
-                    newCell.innerHTML = `${InitialData[i].time}s`
-                }
-                else if (j === 2) {
-                    newCell.innerHTML = `${InitialData[i].name}`
-                }
-                else if (j === 3) {
-                    newCell.innerHTML = `
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-link-45deg" viewBox="0 0 16 16">
-                        <a xlink:href=${InitialData[i].link} target="_blank">
-                            <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z"/>
-                            <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z"/>
-                        </a>
-                    </svg>`
-                }
-                else if (j === 4) {
-                    for (let k = 0; k < InitialData[i].team.length; k++) {
-                        
-                        let icons = document.createElement('img');
-                        icons.src = `assets/img/character-icon2/${InitialData[i].team[k]}.png`;
-                        icons.width = '64';
-                        icons.height = '64';
-                        newCell.append(icons);
+
+            for (let i = 0; i < InitialData.length; i++) {
+                let newRow = new_tbody.insertRow();
+                for (let j = 0; j < 5; j++) {
+                    let newCell = newRow.insertCell(-1);
+
+                    if (j === 0) {
+                        newCell.innerHTML = `${i + 1}`
+                    }
+                    else if (j === 1) {
+                        newCell.innerHTML = `${InitialData[i].time}s`
+                    }
+                    else if (j === 2) {
+                        newCell.innerHTML = `${InitialData[i].name}`
+                    }
+                    else if (j === 3) {
+                        newCell.innerHTML = `
+                    <a href=${InitialData[i].link} target="_blank">
+                        <i class="bi bi-link-45deg"></i>
+                    </a>`
+                    }
+                    else if (j === 4) {
+                        for (let k = 0; k < InitialData[i].team.length; k++) {
+
+                            let icons = document.createElement('img');
+                            icons.src = `assets/img/character-icon2/${InitialData[i].team[k]}.png`;
+                            icons.width = '64';
+                            icons.height = '64';
+                            newCell.append(icons);
+                        }
+
                     }
 
                 }
-
             }
-        }
-        tableRef.parentElement.replaceChild(new_tbody, tableRef);
-        
-    })
-    .catch((error) => {
-        console.log(error);
-    })
+            tableRef.parentElement.replaceChild(new_tbody, tableRef);
+
+        })
+        .catch((error) => {
+            console.log(error);
+        })
 }
 
 
