5bc06784ed6ac42187d7b3d15e91be0b2e936233
AidanXu <aidanxu@Aidans-MBP.lan>
2023-02-13 22:06:57 -0500

    added version selector

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 let currentChamber = document.getElementsByClassName('active')[0];
-console.log(document.referrer);
+let currentVersion = document.getElementsByClassName('selectedVersion')[0];
 
 let navLinks = document.getElementsByClassName('nav-link');
 for (let i = 0; i < navLinks.length; i++) {
@@ -39,6 +39,13 @@ function changeChamber(event) {
         })
         .then(InitialData => {
 
+            //console.log(InitialData);
+            // filter data by abyss version
+            //console.log(currentVersion.innerHTML);
+            const filteredData = InitialData.filter(data => {
+                return data.abyssVersion == currentVersion.innerHTML;
+            })
+            
             // Create new table
             let tableHtml = `
             <table class="table">
@@ -62,7 +69,7 @@ function changeChamber(event) {
 
             let tableRef = document.getElementsByTagName('tbody')[0];
 
-            for (let i = 0; i < InitialData.length; i++) {
+            for (let i = 0; i < filteredData.length; i++) {
                 let newRow = new_tbody.insertRow();
                 for (let j = 0; j < 5; j++) {
                     let newCell = newRow.insertCell(-1);
@@ -71,22 +78,22 @@ function changeChamber(event) {
                         newCell.innerHTML = `${i + 1}`
                     }
                     else if (j === 1) {
-                        newCell.innerHTML = `${InitialData[i].time}s`
+                        newCell.innerHTML = `${filteredData[i].time}s`
                     }
                     else if (j === 2) {
-                        newCell.innerHTML = `${InitialData[i].name}`
+                        newCell.innerHTML = `${filteredData[i].name}`
                     }
                     else if (j === 3) {
                         newCell.innerHTML = `
-                    <a href=${InitialData[i].link} target="_blank">
+                    <a href=${filteredData[i].link} target="_blank">
                         <i class="bi bi-link-45deg"></i>
                     </a>`
                     }
                     else if (j === 4) {
-                        for (let k = 0; k < InitialData[i].team.length; k++) {
+                        for (let k = 0; k < filteredData[i].team.length; k++) {
 
                             let icons = document.createElement('img');
-                            icons.src = `assets/img/character-icon2/${InitialData[i].team[k]}.png`;
+                            icons.src = `assets/img/character-icon2/${filteredData[i].team[k]}.png`;
                             icons.width = '64';
                             icons.height = '64';
                             newCell.append(icons);
@@ -103,3 +110,35 @@ function changeChamber(event) {
             console.log(error);
         })
 }
+
+// dropdown for menu
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.versionMenu');
+    const options = dropdown.querySelectorAll('.versionMenu li');
+    const selected = dropdown.querySelector('.selected');
+
+    select.addEventListener('click', () => {
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('caret-rotate');
+        menu.classList.toggle('versionMenu-open');
+    });
+
+    options.forEach(option => {
+        option.addEventListener('click', () => {
+            selected.innerText = option.innerText;
+            
+            select.classList.remove('select-clicked');
+            caret.classList.remove('caret-rotate');
+            menu.classList.remove('versionMenu-open');
+            options.forEach(option => {
+                option.classList.remove('selectedVersion')
+            })
+            option.classList.add('selectedVersion')
+            currentVersion = document.getElementsByClassName('selectedVersion')[0];
+            changeChamber();
+        })
+    })
+})
